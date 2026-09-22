bfab710ce5d8ea64c0e411df703d760b9d73ad4e
Thiago <thiaguu890@gmail.com>
2026-09-22 09:48:44 -0300

    feat: add global styles and layout for the application
    - Created a new global CSS file to import Tailwind CSS.
    - Implemented a root layout component for the application with a placeholder heading.
    
    feat: create home page component
    
    - Added a home page component with a styled heading.
    
    feat: seed posts data for the application
    
    - Introduced a JSON file to seed initial posts data with various attributes including title, content, and author.
    
    feat: define post model type
    
    - Created a TypeScript model for posts to enforce structure and type safety.

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function homePage() {
-  return <h1>Olá de page.tsx</h1>
-}
-
-//page.tsx = index.html
-// app/page.tsx 
-//app/about/page.tsx → /about
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function homePage() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-blue-500 hover:text-blue-900 hover:bg-blue-500">
+        Texto no H1
+      </h1>
+    </div>
+  );
+}
+
+//page.tsx = index.html
+// app/page.tsx 
+//app/about/page.tsx → /about
