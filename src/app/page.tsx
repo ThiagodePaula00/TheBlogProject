eaef23ea1e65789d7bcc9fe4d7c5fdaaca9fe7b8
Thiago <thiaguu890@gmail.com>
2026-09-24 13:31:20 -0300

    feat: add PostCoverImage and PostHeading components, update home page to use new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { SpinLoader } from "../components/spinLoader";
 import { PostsList } from "../components/PostsList";
 import { Container } from "../components/Container";
 import { Header } from "../components/Header";
-import Link from "next/link";
-import Image from "next/image";
+import { PostHeading } from "../components/PostHeading";
+import { PostCoverImage } from "../components/PostCoverImage";
 
 export default async function homePage() {
 
@@ -13,15 +13,15 @@ export default async function homePage() {
           <Header/>
 
           <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-            <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-              <Image className="w-full h-full object-cover object-center group-hover:scale-105 transition" src='/imagens/bryen_0.png' width={1200} height={720} alt="Título do post" priority />
-            </Link>
+
+              <PostCoverImage linkProps={{ href: '/post/dfsgdfsg' }} imageProps={{ width: 1200, height: 700, src: 'imagens/bryen_9.png', alt:'Alt da image', priority: true}} />
+
             <div className="flex flex-col gap-4 sm:justify-center">
               <time className="text-slate-600 block text-sm/tight" dateTime="2026-09-24">23/09/26 10:00</time>
 
-              <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
-                <Link href="#">Lorem ipsum dolor sit amet.</Link>
-              </h1>
+              <PostHeading as='h1' url="#">
+                Lorem, ipsum dolor.
+              </PostHeading>
 
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, possimus optio magnam tempore unde tenetur quod praesentium rem! Commodi nam rem velit vitae distinctio ad debitis beatae nihil porro libero?
@@ -42,5 +42,5 @@ export default async function homePage() {
   );
 }
 
-// x e y se referem a eixos. px é padding no eixo x e py é padding no eixo y, por exemplo
+// x e y se referem a eixos. px é padding no eixo x e py é padding no eixo y, por exemplo.
 /* dark:bg-slate-900 dark:text-slate-100 */
