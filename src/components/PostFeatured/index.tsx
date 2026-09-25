db1ddd4ca28499ad4d3bdf674321cbc75c4e5042
Thiago <thiaguu890@gmail.com>
2026-09-25 18:15:10 -0300

    feat: refactor post retrieval to use findAllPublic and implement caching for public posts

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,26 +1,19 @@
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/src/lib/post/queries";
 
-export function PostFeatured() {
-    const slug = 'qualquer';
-    const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+    const posts = await findAllPublicPosts();
+    const post = posts[0];
+
+    const postLink = `/post/${post.slug}`;
     
     return (
           <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
 
-              <PostCoverImage linkProps={{ href: postLink }} imageProps={{ width: 1200, height: 700, src: '/imagens/bryen_9.png', alt:'Alt da image', priority: true}} />
+              <PostCoverImage linkProps={{ href: postLink }} imageProps={{ width: 1200, height: 700, src: post.coverImageUrl, alt: post.title, priority: true}} />
 
-            <div className="flex flex-col gap-4 sm:justify-center">
-              <time className="text-slate-600 block text-sm/tight" dateTime="2026-09-24">23/09/26 10:00</time>
-
-              <PostHeading as='h1' url={postLink}>
-                Lorem, ipsum dolor.
-              </PostHeading>
-
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, possimus optio magnam tempore unde tenetur quod praesentium rem! Commodi nam rem velit vitae distinctio ad debitis beatae nihil porro libero?
-              </p>
-            </div>
+                <PostSummary  postLink={postLink} postHeading='h1' createdAt={post.createdAt} excerpt={post.excerpt}title={post.title} />
           </section>
     )
 }
