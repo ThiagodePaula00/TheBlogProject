f99c1ba132efe357acd4823675555b7fe337759f
Thiago <thiaguu890@gmail.com>
2026-09-25 11:55:24 -0300

    feat: refactor PostsList to use PostSummary component and update date formatting utilities

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,6 @@
 import { postRepository } from "@/src/repository/post";
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
+import { PostSummary } from "../PostSummary";
 
 export async function PostsList() {
     const posts = await postRepository.findAll();
@@ -13,17 +13,7 @@ export async function PostsList() {
             return (<div className="flex flex-col gap-4 group" key={post.id}>
                 <PostCoverImage linkProps={{ href: postLink }} imageProps={{ width: 1200, height: 720, src: post.coverImageUrl, alt: post.title}} />
 
-                <div className="flex flex-col gap-4 sm:justify-center">
-                    <time className="text-slate-600 block text-sm/tight" dateTime={post.createdAt}> {post.createdAt}</time>
-
-                    <PostHeading as='h2' url={postLink}>
-                        {post.title}
-                    </PostHeading>
-
-                    <p>
-                        {post.excerpt}
-                    </p>
-                </div>
+                <PostSummary postLink={postLink} postHeading="h1" createdAt={post.createdAt} excerpt={post.excerpt} title={post.title} />
             </div>
             );
             })}
